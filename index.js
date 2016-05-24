922e5db6e16d59ad1152d8a1a51ee258eb0d6dfa
Rémy MEJA <remy.meja@inist.fr>
2016-05-24 08:10:10 +0200

    Test with nodehun and dictionnaries from LibreOffice

--- index.js
+++ index.js
@@ -4,36 +4,62 @@ const natural = require('natural'),
   bluebird = require('bluebird'),
   fs = bluebird.promisifyAll(require('fs')),
   tokenizer = new natural.WordTokenizer(),
-  dict = fs.readFileSync(__dirname + '/assets/dict/en/general.txt', 'utf8'),
   kuler = require('kuler');
+
 
 fs.readFileAsync(__dirname + '/test/test.txt', 'utf8').then((text) => {
   // console.log(tokenizer.tokenize(text));
   return tokenizer.tokenize(text)
 }).then((tokens) => {
-  const result = {
-    valid: 0,
-    error: 0
-  };
+  const dict = fs.readFileSync(__dirname + '/assets/dict/en/general.txt', 'utf8'),
+    result = {
+      valid: 0,
+      error: 0
+    };
   tokens.forEach((word) => {
-    const regex = new RegExp('\\b'+word+'\\b');
+    const regex = new RegExp('\\b' + word + '\\b');
 
     if (dict.match(regex)) {
-      console.log(word, kuler('OK', 'green'));
+      // console.log(word, kuler('OK', 'green'));
       result.valid++;
     } else {
-      console.log(word, kuler('KO', 'red'))
+      // console.log(word, kuler('KO', 'red'));
       result.error++
     }
   });
   return result
 }).then((result) => {
-  console.log(Math.trunc(result.valid/(result.error+result.valid)*100) + "%");
+  console.log('quality text :', Math.trunc(result.valid / (result.error + result.valid) * 100) + "%");
 });
 
 
+// With nodehun + dictionnaries from LibreOffice
 
+const nodehun = require('nodehun');
 
+fs.readFileAsync(__dirname + '/test/test.txt', 'utf8').then((text) => {
+  // console.log(tokenizer.tokenize(text));
+  return tokenizer.tokenize(text)
+}).then((tokens) => {
+  const dict = new nodehun(fs.readFileSync(__dirname + '/assets/dict-hunspell/en/en_US.aff'), fs.readFileSync(__dirname + '/assets/dict-hunspell/en/en_US.dic'));
+  const result = {
+    valid: 0,
+    error: 0
+  };
+
+  tokens.forEach((word) => {
+    if (dict.isCorrectSync(word)) {
+      // console.log(word, kuler('OK', 'green'));
+      result.valid++;
+    } else {
+      // console.log(word, kuler('KO', 'red'));
+      result.error++
+    }
+  });
+  return result
+}).then((result) => {
+  console.log('quality text with nodehun + dictionnaries from LibreOffice :', Math.trunc(result.valid / (result.error + result.valid) * 100) + "%");
+});
 
 
 
