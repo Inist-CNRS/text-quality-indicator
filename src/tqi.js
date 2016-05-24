0db39d74e172c35f7a0aeda441556acebc706d6d
mattd <matthias.dieudonne@gmail.com>
2016-05-24 16:53:42 +0200

    Use async & start Tests

--- src/tqi.js
+++ src/tqi.js
@@ -3,16 +3,19 @@
 const natural = require('natural'),
   bluebird = require('bluebird'),
   fs = bluebird.promisifyAll(require('fs')),
+  path = require('path'),
   tokenizer = new natural.WordTokenizer(),
+  async = require('async'),
   nodehun = require('nodehun');
 
 class Tqi {
+
   constructor(dic, aff) {
-    this._dic = dic || __dirname + '/../assets/dict-hunspell/en/en_US.dic';
-    this._aff = aff || __dirname + '/../assets/dict-hunspell/en/en_US.aff';
+    this._dic = dic || path.resolve(__dirname + '/../assets/dict-hunspell/en/en_US.dic');
+    this._aff = aff || path.resolve(__dirname + '/../assets/dict-hunspell/en/en_US.aff');
   }
 
-  analyze(text) {
+  analyze(text,callback) {
     const tokens = tokenizer.tokenize(text);
     // TODO : Gérer les cas de chemins invalides vers les fichiers dic et aff
     const dict = new nodehun(fs.readFileSync(this._aff), fs.readFileSync(this._dic));
@@ -22,15 +25,22 @@ class Tqi {
       rate: 0
     };
 
-    tokens.forEach((word) => {
+    async.each(tokens, (word,next) => {
       if (dict.isCorrectSync(word)) {
         result.valid++;
       } else {
-        result.error++
+        result.error++;
       }
+      //next word
+      next();
+    } , (err) => {
+      if(err){
+        throw err;
+      }
+      result.rate = result.valid / (result.error + result.valid) * 100;
+      //Return result from module
+      callback(result);
     });
-    result.rate = result.valid / (result.error + result.valid) * 100;
-    return result
   }
 }
 
--- test/tqi-test.js
+++ test/tqi-test.js
@@ -3,12 +3,22 @@
 const pkg = require('./../package.json'),
   Tqi = require('./../src/tqi'),
   chai = require('chai'),
+  fs = require('fs'),
   expect = chai.expect;
 
+var tqio = new Tqi();
+
 describe(pkg.name + '/src/tqi.js', () => {
-  describe('#constructor', () => {
-    it('should initialize something, non ?', () => {
-      // TODO : Il faut des test unitaires
-    })
-  })
+  describe('#Constructor', () => {
+    it('Must have defaults path in .dic & .aff', () => {
+    	expect(tqio._dic).to.be.a('string');
+    	expect(tqio._aff).to.be.a('string');
+    });
+    it('Paths from constructor should exist', () => {
+    	var dic = fs.accessSync(tqio._dic, fs.R_OK);
+    	var aff = fs.accessSync(tqio._dic, fs.R_OK);
+    	expect(dic).to.be.equal(undefined);
+    	expect(aff).to.be.equal(undefined);
+    });
+  });
 });
