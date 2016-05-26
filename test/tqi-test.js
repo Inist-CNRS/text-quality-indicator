14eb63dfa608a2ffdb43ee70aefa043230a6dcb0
Rémy MEJA <remy.meja@univ-lorraine.fr>
2016-05-26 21:00:43 +0200

    Update unit test

--- test/tqi-test.js
+++ test/tqi-test.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const pkg = require('./../package.json'),
   Tqi = require('./../src/tqi'),
@@ -8,33 +8,41 @@ const pkg = require('./../package.json'),
   expect = chai.expect;
 
 var tqi = new Tqi(),
-		enBig = fs.readFileSync(path.resolve(__dirname + '/data/en-big.txt'), 'utf8');
+  enBig = fs.readFileSync(path.resolve(__dirname + '/data/en-big.txt'), 'utf8');
 
-describe(pkg.name + '/src/tqi.js', () => {
-  describe('#Constructor', () => {
+describe(pkg.name + '/src/tqi.js', function () {
+  describe('#Constructor', function () {
     it('Must have defaults path in .dic & .aff', () => {
       expect(tqi._dic).to.be.a('string');
       expect(tqi._aff).to.be.a('string');
     });
-    it('Paths from constructor should exist', (done) => {
-    	fs.access(tqi._dic, fs.R_OK, (err) => {
-			  expect(err).to.be.null;
-			  fs.access(tqi._aff, fs.R_OK, (err) => {
-			  	expect(err).to.be.null;
-				  done();
-				});
-			});
+    it('Dic\'s path from constructor should exist', function (done) {
+      fs.access(tqi._dic, fs.R_OK, (err) => {
+        expect(err).to.be.null;
+        done();
+      });
+    });
+    it('Aff\'s path from constructor should exist', function (done) {
+      fs.access(tqi._aff, fs.R_OK, (err) => {
+        expect(err).to.be.null;
+        done();
+      });
     });
   });
-  describe('#Analyze', () => {
-  	it('Analyze must return exact int numbers', function() {
-  		this.timeout = 5500;
-			return tqi.analyze(enBig).then(function(result){
-			  expect(result.valid).to.be.a("number");
-			  expect(result.error).to.be.a("number");
-			  expect(result.rate).to.be.a("number");
-			  expect(result.valid + result.error).to.be.equal(1454145);
-			});
-  	});
+  describe('#Analyze', function () {
+    this.timeout(5500);
+    it('Analyze must return an object "result"', function (done) {
+      tqi.analyze(enBig).then((result) => {
+        expect(result).to.have.property("valid");
+        expect(result.valid).to.be.a("number");
+        expect(result).to.have.property("error");
+        expect(result.error).to.be.a("number");
+        expect(result).to.have.property("rate");
+        expect(result.rate).to.be.a("number");
+        done();
+      }, (err) => {
+        done(err);
+      })
+    })
   });
 });
