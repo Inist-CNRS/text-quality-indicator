2947abc240f81e8987a8fa3ae61f52f2208621cb
Rémy MEJA <remy.meja@inist.fr>
2016-09-22 11:46:04 +0200

    Mise jour du code suite a l'abandon de nodehun

--- bin/cli.js
+++ bin/cli.js
@@ -24,36 +24,37 @@ fs.statAsync(program.args[0]).catch((err) => {
   console.log(kuler('Input file/folder doesn\'t exist', 'red'));
   process.exit(1);
 }).then((stats) => {
-  const analyzeThisFile = (file) => {
-    const options = { words : program.words };
-    return fs.readFileAsync(file, 'utf8').then((data) => {
-      const tqiDE = new Tqi(program.lang)
-      return tqiDE.analyze(data,options);
-    })
-  };
-
-  if (stats.isFile()) {
-    const input = path.resolve(program.args[0]);
-    analyzeThisFile(input).then((result) => {
-      console.log(path.basename(input), "=>", result);
-    });
-  } else {
-    const total = {valid: 0, error: 0, rate: 0},
-      input = path.resolve(program.args[0], "**/*.txt"),
-      arrayFiles = glob.sync(input);
-
-    async.each(arrayFiles, (file, next) => {
-      analyzeThisFile(file).then((result) => {
-        total.valid += result.valid;
-        total.error += result.error;
-        console.log(path.basename(file), "=>", result);
-        next();
-      });
-    }, (err) => {
-      if (arrayFiles.length !== 1) {
-        total.rate = total.valid / (total.error + total.valid) * 100;
-        console.log("total =>", total);
-      }
-    });
-  }
+  console.log('I need compassion and some update');
+  // const analyzeThisFile = (file) => {
+  //   const options = { words : program.words };
+  //   return fs.readFileAsync(file, 'utf8').then((data) => {
+  //     const tqiDE = new Tqi(program.lang);
+  //     return tqiDE.analyze(data,options);
+  //   })
+  // };
+  //
+  // if (stats.isFile()) {
+  //   const input = path.resolve(program.args[0]);
+  //   analyzeThisFile(input).then((result) => {
+  //     console.log(path.basename(input), "=>", result);
+  //   });
+  // } else {
+  //   const total = {valid: 0, error: 0, rate: 0},
+  //     input = path.resolve(program.args[0], "**/*.txt"),
+  //     arrayFiles = glob.sync(input);
+  //
+  //   async.each(arrayFiles, (file, next) => {
+  //     analyzeThisFile(file).then((result) => {
+  //       total.valid += result.valid;
+  //       total.error += result.error;
+  //       console.log(path.basename(file), "=>", result);
+  //       next();
+  //     });
+  //   }, (err) => {
+  //     if (arrayFiles.length !== 1) {
+  //       total.rate = total.valid / (total.error + total.valid) * 100;
+  //       console.log("total =>", total);
+  //     }
+  //   });
+  // }
 });
--- test/tqi-test.js
+++ test/tqi-test.js
@@ -2,79 +2,72 @@
 
 const pkg = require('./../package.json'),
   Tqi = require('./../src/tqi'),
-  chai = require('chai'),
-  path = require('path'),
   fs = require('fs'),
+  chai = require('chai'),
   expect = chai.expect;
 
 
-var tqi = new Tqi(),
-  enBig = fs.readFileSync(path.resolve(__dirname + '/data/en-big.txt'), 'utf8'),
-  emptyFile = fs.readFileSync(path.resolve(__dirname + '/data/empty-file.txt'), 'utf8');
+const tqi = new Tqi(),
+  enBig = __dirname + '/data/en-big.txt',
+  emptyFile = __dirname + '/data/empty-file.txt',
+  frSample = __dirname + '/data/fr-sample.txt';
 
 
 describe(pkg.name + '/src/tqi.js', function () {
-  describe('#Constructor', function () {
-    it('Dictionaries have to be in node_modules', function (done) {
-      fs.access(path.resolve("node_modules/dictionaries/"), fs.R_OK, (err) => {
-        expect(err).to.be.null;
-        done();
-      });
-    });
-    it('Must throw error when bad path .dic sent', function (done) {
-      try {
-        var badTqi = new Tqi(null,'a/very/bad/path.dic', 'a/very/bad/path.aff');
-      } catch(error) {
-          done();
-      }
-    });
-    it('Must have English as default language', () => {
-      expect(tqi._langs).to.be.a('string');
-      expect(tqi._langs).to.be.eql('en');
-    });
-    it('Dictionary from constructor should exist', function (done) {
-      expect(tqi._firstDict).to.have.property('dic');
-      expect(tqi._firstDict).to.have.property('aff');
-      done()
-    });
+  describe('#constructor', function () {
+    it('should initialize dictionnary', function () {
+      expect(tqi.dict).to.be.an('array')
+    })
   });
-  describe('#Analyze', function () {
-    this.timeout(6900);
-    it('Analyze must return an object "result"', function (done) {
-      tqi.analyze(enBig).then((result) => {
-        expect(result).to.have.property("valid");
-        expect(result.valid).to.be.a("number");
-        expect(result).to.have.property("error");
-        expect(result.error).to.be.a("number");
+
+  describe('#getCorrectWord', function () {
+    it('should return a total of correct word', function () {
+      return tqi.getCorrectWord(frSample).then((result) => {
+        // console.log(result);
+        expect(result).to.be.an("array");
+      })
+    })
+  });
+
+  describe('#getMispelledWord', function () {
+    it('should return a total of mispelled word', function () {
+      return tqi.getMispelledWord(frSample).then((result) => {
+        expect(result).to.be.an("array");
+      })
+    })
+  });
+
+  describe('#analyze', function () {
+    it('should return an object "result"', function () {
+      return tqi.analyze(frSample).then((result) => {
+        // console.log(result);
+        expect(result).to.have.property("correct");
+        expect(result.correct).to.be.a("number");
+        expect(result).to.have.property("mispelled");
+        expect(result.mispelled).to.be.a("number");
         expect(result).to.have.property("rate");
         expect(result.rate).to.be.a("number");
-        done();
-      }, (err) => {
-        done(err);
-      })
-    });
-    it('Analyze must return an object "result" even with an empty file', function (done) {
-      tqi.analyze(emptyFile).then((result) => {
-        expect(result).to.have.property("valid");
-        expect(result.valid).to.be.a("number");
-        expect(result).to.have.property("error");
-        expect(result.error).to.be.a("number");
-        expect(result).to.have.property("rate");
-        expect(result.rate).to.be.a("number");
-        done();
-      }, (err) => {
-        done(err);
-      })
-    });
-    it('Analyze must return zero found result when bad language used', function (done) {
-      tqi.analyze("chien poisson").then((result) => {
-        expect(result.valid).to.be.eql(0);
-        expect(result.error).to.be.eql(2);
-        done();
-      }, (err) => {
-        done(err);
       })
     });
 
+    it('should return an object "result" even with an empty file', function () {
+      return tqi.analyze(emptyFile).then((result) => {
+        // console.log(result);
+        expect(result).to.have.property("correct");
+        expect(result.correct).to.be.a("number");
+        expect(result).to.have.property("mispelled");
+        expect(result.mispelled).to.be.a("number");
+        expect(result).to.have.property("rate");
+        expect(result.rate).to.be.a("number");
+      })
+    })
+  });
+
+  describe('#spawnCmdHunspell', function () {
+    it('should return an object "result"', function () {
+      return tqi.spawnCmdHunspell(['-G', '/home/meja/Dev/Github/text-quality-indicator/test/data/en-sample.txt']).then((result) =>{
+        // console.log(result);
+      })
+    })
   });
 });
